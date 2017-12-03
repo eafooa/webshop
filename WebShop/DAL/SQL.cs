a0f090f38e2b2f748cea4464318ca3647b2a116e
Omar Akbar <eafooa@yahoo.com>
2017-12-03 16:21:49 -0500

    Added DB Api Reader

---
+++ WebShop/DAL/SQL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebShop.DAL
+{
+    public static class SQL
+    {
+        public const string Users = @" SELECT
+                                            Id,
+                                            Email,
+                                            PasswordHash,
+                                            SecurityStamp,
+                                            PhoneNumber,
+                                            UserName
+                                       FROM AspNetUsers";
+    }
+}
